97d882cd5fe02de1cc19f177348ad83e42745fa4
Donald Lee <donaldturinglee@gmail.com>
2023-11-18 23:32:28 +0800

    feat: Optimizing code

--- 26.remove-duplicates-from-sorted-array.cpp
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -2,10 +2,10 @@ int removeDuplicates(vector<int>& nums) {
     int k = 0;
 
     for (int i = 1; i < nums.size(); ++i) {
-        if (nums[i - 1] != nums[i]) {
-            nums[++k] = nums[i];
+        if (k < 1 || nums[i - 1] != nums[i]) {
+            nums[k++] = nums[i];
         }
     }
 
-    return k + 1;
+    return k;
 }
