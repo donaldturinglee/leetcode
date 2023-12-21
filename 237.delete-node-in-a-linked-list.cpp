793863d6197ffa96428401099f1942b789bad4cb
Donald Lee <donaldturinglee@gmail.com>
2023-12-20 22:08:21 -0800

    feat: Add 237.delete-node-in-a-linked-list

---
+++ 237.delete-node-in-a-linked-list.cpp
@@ -0,0 +1,7 @@
+void deleteNode(ListNode* node) {
+    if (node == nullptr || node->next == nullptr) return;
+    node->val = node->next->val;
+    ListNode* temp = node->next;
+    node->next = node->next->next;
+    delete temp;
+}
